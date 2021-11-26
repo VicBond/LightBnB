d1505abf614393fd3f409e463ca72b0767ef675f
Victor Bondarenko <nirasimha@gmail.com>
2021-11-26 00:42:49 +0000

    function to save a new property to the properties table added

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,4 @@
-/* eslint-disable camelcase */
-// const properties = require('./json/properties.json');
-// const users = require('./json/users.json');
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -11,11 +9,7 @@ const pool = new Pool({
 });
 /// Users
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+
 const getUserWithEmail = function(email) {
   return pool.query(`
   SELECT *
@@ -26,11 +20,7 @@ const getUserWithEmail = function(email) {
 };
 exports.getUserWithEmail = getUserWithEmail;
 
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+
 const getUserWithId = function(id) {
   return pool.query(`
   SELECT *
@@ -42,11 +32,7 @@ const getUserWithId = function(id) {
 exports.getUserWithId = getUserWithId;
 
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
+
 const addUser =  function(user) {
   return pool.query(`
     INSERT INTO users (name, email, password)
@@ -59,11 +45,6 @@ exports.addUser = addUser;
 
 /// Reservations
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool.query(`
   SELECT *
@@ -79,12 +60,7 @@ exports.getAllReservations = getAllReservations;
 
 /// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
+
 const getAllProperties = function(options, limit = 10) {
   const queryParams = [];
 
@@ -147,15 +123,41 @@ const getAllProperties = function(options, limit = 10) {
 exports.getAllProperties = getAllProperties;
 
 
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
-// exports.addProperty = addProperty;
+
+const addProperty = function(property) {
+  return pool.query(`
+  INSERT INTO properties 
+  (owner_id,
+    title,
+     description,
+      thumbnail_photo_url,
+       cover_photo_url,
+        cost_per_night,
+         parking_spaces,
+          number_of_bathrooms,
+           number_of_bedrooms,
+            country,
+             street,
+              city,
+               province,
+                post_code) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *`,[
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code])
+    .then(res => res.rows[0])
+    .catch(err => err);
+};
+exports.addProperty = addProperty;
